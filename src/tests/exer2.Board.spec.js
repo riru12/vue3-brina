226d6bc92d37cfba56cc9460bf0354e4000b5e79
riru12 <brilljulianrina@gmail.com>
2025-04-08 09:38:59 +0800

    exercise2/Board.vue testing

--- src/tests/exer2.Board.spec.js
+++ src/tests/exer2.Board.spec.js
@@ -54,4 +54,30 @@ describe('Board.vue', () => {
     const listItems = boardItem.findAll('li')
     expect(listItems).toHaveLength(3)
   })
+
+  it('changes board color', () => {
+    function hexToRgb(hex) {
+      var bigint = parseInt(hex, 16);
+      var r = (bigint >> 16) & 255;
+      var g = (bigint >> 8) & 255;
+      var b = bigint & 255;
+  
+      return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+  
+    const wrapper = mountFactory(props);
+    const colorSelect = wrapper.find('select');
+  
+    const selectedOption = colorSelect.findAll('option').at(1);
+    colorSelect.element.value = selectedOption.element.value;
+    const expectedColor = selectedOption.element.value;
+    colorSelect.trigger('change');  // trigger color change on component
+    
+    // extract color of <h6> tag
+    const actualColor = wrapper.find('h6').element.style.color;
+  
+    // Compare the colors
+    expect(actualColor).toBe(hexToRgb(expectedColor));
+  });
+  
 })
